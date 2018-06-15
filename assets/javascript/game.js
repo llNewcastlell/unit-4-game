fb1a477d5e681d33affce02d05fe20574790762b
Newcastle <vaughnmarques@gmail.com>
2018-06-14 18:28:38 -0600

    last push Thursday

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,118 @@
-var jewelNum = "";
-$( document ).ready(function() {
-    $(".card-body-1").on("click", function(){
-        // var jewelNum = "";
+$(document).ready(function () {
+    var loss = 0;
+    var wins = 0;
+    var crystalone;
+    var crystaltwo;
+    var crystalthree;
+    var crystalfour;
+    var jewelNum;
+    var score = 0;
+    var counter = 0;
+    // This will rest the game and provide each jewel with a new number
+    function startgame() {
 
-        for (var i = 0; i<2; i++){
-            var random = Math.floor(Math.random() * 4) +1;
-            jewelNum = random + jewelNum;
-        }
-        console.log("number" + jewelNum);
-        // $(".card-text-score").append(""+ jewelNum);
+        crystalone = Math.floor((Math.random() * 10) + 1);
+        $(".card-img-top-1").attr("data-jewel-value", crystalone);
+
+        crystaltwo = Math.floor((Math.random() * 10) + 1);
+        $(".card-img-top-2").attr("data-jewel-value", crystaltwo);
+
+        crystalthree = Math.floor((Math.random() * 10) + 1);
+        $(".card-img-top-3").attr("data-jewel-value", crystalthree);
+
+        crystalfour = Math.floor((Math.random() * 10) + 1);
+        $(".card-img-top-4").attr("data-jewel-value", crystalfour);
+
+        jewelNum = Math.floor((Math.random() * 101) + 19);
+
+        $(".card-text-wins").text("Wins: " + wins);
+        $(".card-text-losses").text("Losses: " + loss);
+        $(".card-text-score").text("Score: " + jewelNum);
+        $(".card-header-total").text("Your Total Score: " + score)
+        console.log(crystalone);
+        console.log(crystaltwo);
+        console.log(crystalthree);
+        console.log(crystalfour);
+
+    }
+
+
+
+    startgame();
+    // console.log("-------------" + jewelNum);
+
+    // generate a number for each jewel
+    $("#gems1").on("click", function () {
+        var jewelValue = ($(this).attr("data-jewel-value"));
+        jewelValue = parseInt(jewelValue);
+        counter += jewelValue;
+        $(".card-header-total").text("Your Total Score: " + counter);
+
+        jewelLogic();
+
+
     })
 
-    $(".card-body-2").on("click", function(){
-        // var jewelNum = "";
+    $("#gems2").on("click", function () {
+        var jewelValue = ($(this).attr("data-jewel-value"));
+        jewelValue = parseInt(jewelValue);
+        counter += jewelValue;
+        $(".card-header-total").text("Your Total Score: " + counter);
 
-        for (var i = 0; i<2; i++){
-            var random = Math.floor(Math.random() * 4) +1;
-            jewelNum = random + jewelNum;
-        }
-        console.log("number" + jewelNum);
-        // $(".card-text-score").append(""+ jewelNum);
+
+        jewelLogic();
+
     })
-    
+
+
+    $("#gems3").on("click", function () {
+        var jewelValue = ($(this).attr("data-jewel-value"));
+        jewelValue = parseInt(jewelValue);
+        counter += jewelValue;
+        $(".card-header-total").text("Your Total Score: " + counter);
+
+
+        jewelLogic();
+
+    })
+
+
+    $("#gems4").on("click", function () {
+        var jewelValue = ($(this).attr("data-jewel-value"));
+        jewelValue = parseInt(jewelValue);
+        counter += jewelValue;
+        $(".card-header-total").text("Your Total Score: " + counter);
+
+
+        jewelLogic();
+
+    })
+
+    console.log("jewel ddddddddd" + jewelNum);
+
+    function jewelLogic() {
+        if (counter === jewelNum) {
+            wins++;
+            $(".card-text-wins").text("Wins: " + wins);
+            alert("You win");
+            startgame();
+
+        } else if (counter >= jewelNum) {
+            loss--;
+            $(".card-text-losses").text("Losses: " + loss);
+            alert("You lost");
+            
+            startgame();
+        }
+
+    }
+
+
+
+
+    // document.querySelector("#card-text-wins").innerHTML = "Wins: " + wins;
+
+
+
+
 });
-
